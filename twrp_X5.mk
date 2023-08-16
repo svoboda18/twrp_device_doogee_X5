ca1748126134bf55e870c2b7ac4b898dfa4e0560
svoboda18 <dev.svoboda18@gmail.com>
2023-08-16 19:00:00 +0000

    X5: drop fastbootd for now
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- twrp_X5.mk
+++ twrp_X5.mk
@@ -22,10 +22,6 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-#
-# # Fastbootd
-PRODUCT_PACKAGES += \
-	android.hardware.fastboot@1.0-impl-mock
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_X5-userdebug 10 QP1A.190711.020 mp1V91221 release-keys"
