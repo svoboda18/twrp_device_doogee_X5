7477640aee82385dbd2e37a158c1ee3ca586a8b3
svoboda18 <dev.svoboda18@gmail.com>
2023-08-20 03:08:16 +0200

    X5: build `health@2.0` hal and use minimal vintf manifest
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- twrp_X5.mk
+++ twrp_X5.mk
@@ -7,7 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := X5
@@ -16,12 +16,15 @@ PRODUCT_BRAND := DOOGEE
 PRODUCT_MODEL := X5
 PRODUCT_MANUFACTURER := DOOGEE
 
-PRODUCT_GMS_CLIENTID_BASE := android-doogee
-
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-impl
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_X5-userdebug 10 QP1A.190711.020 mp1V91221 release-keys"
